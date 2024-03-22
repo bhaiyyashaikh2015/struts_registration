16d6249f0ed153306d2cbf10f0e70117f2ca9c1a
unknown <bhaiyyashaikh2015@gmail.com>
2024-03-22 13:05:56 +0530

    initial commit

---
+++ src/main/java/com/ex/ShowFormServlet.java
@@ -0,0 +1,27 @@
+package com.ex;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @email Ramesh Fadatare
+ */
+
+@WebServlet("/")
+public class ShowFormServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("Inside doGet()...");
+		response.sendRedirect("employeeregister.jsp");
+	}
+}
